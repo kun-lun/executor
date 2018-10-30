10e2624c2bd7cf5694433064bfb647ca56d358d2
Andy Liu <andyliuliming@outlook.com>
2018-10-30 22:07:30 +0800

    command set.

--- commands/command.go
+++ commands/command.go
@@ -1,6 +1,10 @@
 package commands
 
-import "github.com/xplaceholder/common/storage"
+import (
+	"github.com/xplaceholder/common/storage"
+)
+
+type CommandSet map[string]Command
 
 type Command interface {
 	CheckFastFails(subcommandFlags []string, state storage.State) error
