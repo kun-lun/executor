beab888920ac1712aaa9d4c70b4461480e52a26f
Andy Liu <andyliuliming@outlook.com>
2018-11-09 21:23:46 +0800

    add play deployment test case.

---
+++ commands/apply_deployment_test.go
@@ -0,0 +1,12 @@
+package commands_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/kun-lun/executor/commands"
+)
+
+var _ = Describe("ApplyDeployment", func() {
+
+})
